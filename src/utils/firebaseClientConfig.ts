aa1c077c952b387843965251887c2b1b55ac86ad
leosaponisouza <72236233+leosaponisouza@users.noreply.github.com>
2025-02-27 10:45:44 -0300

    fixing

--- src/utils/firebaseClientConfig.ts
+++ src/utils/firebaseClientConfig.ts
@@ -1,24 +1,22 @@
 // src/utils/firebaseClientConfig.ts
 import { initializeApp } from 'firebase/app';
 import * as firebaseAuth from 'firebase/auth';
-// Importe getReactNativePersistence do local correto: // <--- CORREÇÃO
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
+import Config from 'react-native-config';
 
-// ... resto do código ...
 const firebaseConfig = {
-    apiKey: "AIzaSyBC6tdEgcM5t3U_ct0aAYSr3kJ4DGZkNX0",
-    authDomain: "rephelper.firebaseapp.com",
-    projectId: "rephelper",
-    storageBucket: "rephelper.firebasestorage.app",
-    messagingSenderId: "717278801852",
-    appId: "1:717278801852:web:c24a9cd864ea92329745b9"
-  }; //Sua configuração
+  apiKey: Config.FIREBASE_API_KEY,
+  authDomain: Config.FIREBASE_AUTH_DOMAIN,
+  projectId: Config.FIREBASE_PROJECT_ID,
+  storageBucket: Config.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Config.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Config.FIREBASE_APP_ID
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage) // Agora funciona
+  persistence: getReactNativePersistence(AsyncStorage)
 });
-
 
 export { auth };
